2f68f6a1544d99dc90c02a5f85f89673c65bbaa5
sxh705 <2362989228@qq.com>
2024-01-05 22:40:42 +0800

    恢复

--- src/main/java/io/github/sxh705/async/AsyncController.java
+++ src/main/java/io/github/sxh705/async/AsyncController.java
@@ -15,7 +15,7 @@ import java.util.concurrent.CountDownLatch;
 public class AsyncController {
     @Autowired
     AsyncService asyncService;
-
+    // 
     @GetMapping("/vt")
     public String vt() {
         long start = System.currentTimeMillis();
--- src/main/java/io/github/sxh705/config/ThreadConfig.java
+++ src/main/java/io/github/sxh705/config/ThreadConfig.java
@@ -1,5 +1,6 @@
 package io.github.sxh705.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +10,7 @@ import org.springframework.core.task.support.TaskExecutorAdapter;
 import java.util.concurrent.Executors;
 
 @Configuration
+@Slf4j
 @ConditionalOnProperty(value = "spring.thread-executor", havingValue = "virtual")
 public class ThreadConfig {
 
@@ -20,6 +22,7 @@ public class ThreadConfig {
     @Bean
     public TomcatProtocolHandlerCustomizer<?> protocolHandlerCustomizer() {
         return protocolHandler -> {
+            log.info("111");
             protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
         };
     }
